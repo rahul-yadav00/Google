5fb925c37f42851d0ec2dd8fd7389c28912ca91c
rahul-yadav00 <125676281+rahul-yadav00@users.noreply.github.com>
2023-02-28 16:39:32 +0530

    first commit

---
+++ Dutchman Files/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    menuokl();
+  });
+  
+  function menuokl() {
+    var menuContent = "";
+    for (i = 0; i < 200; i++) {
+      menuContent += `<div class="menu__content">
+              <h3 class="menu__article_id">${DB2.spirits[i].artikelid}</h3>
+              <h3 class="menu__name">${DB2.spirits[i].namn}</h3>
+              <span class="menu__detail">${DB2.spirits[i].alkoholhalt}</span>
+              <span class="menu__preci">${DB2.spirits[i].prisinklmoms}</span>
+              <a href="#" class="button menu__button">Order<i class='bx bx-cart-alt'></i></a>
+      </div>`;
+    }
+    // console.log(menuContent);
+    $("#menu_container").html(menuContent);
+  }
+  
